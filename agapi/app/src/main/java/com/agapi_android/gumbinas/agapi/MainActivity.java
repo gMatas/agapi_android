61b0ff295128f0895336e003a65651a3e4e34b39
Matas Gumbinas <matas.gumbinas@gmail.com>
2018-04-29 16:19:21 +0300

    Add main activity

--- agapi/app/src/main/java/com/agapi_android/gumbinas/agapi/MainActivity.java
+++ agapi/app/src/main/java/com/agapi_android/gumbinas/agapi/MainActivity.java
@@ -1,13 +1,44 @@
 package com.agapi_android.gumbinas.agapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_my_profile:
+                    mTextMessage.setText(R.string.title_my_profile);
+                    return true;
+                case R.id.navigation_contacts:
+                    mTextMessage.setText(R.string.title_contacts);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
