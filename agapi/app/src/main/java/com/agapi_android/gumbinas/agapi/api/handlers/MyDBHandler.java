23dfee7290c4f5bdb2db226e8000fdc0b84f9c6a
Matas Gumbinas <matas.gumbinas@gmail.com>
2018-05-01 00:10:11 +0300

    Extend SQLiteOpenHelper

--- agapi/app/src/main/java/com/agapi_android/gumbinas/agapi/api/handlers/MyDBHandler.java
+++ agapi/app/src/main/java/com/agapi_android/gumbinas/agapi/api/handlers/MyDBHandler.java
@@ -1,4 +1,23 @@
 package com.agapi_android.gumbinas.agapi.api.handlers;
 
-public class MyDBHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
